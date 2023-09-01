9a0c14aa5200b766ff13695b6f52af4bbb11ae1c
Krishna Chaitanya Surapaneni <chaitanya1004@gmail.com>
2023-09-01 00:27:08 -0600

    main method is added, as this is the starting point to start the application by enabling Testcontainers at development time by attaching container configuration and calling actual application class

--- src/test/java/com/learn/jpa/JpaApplicationTests.java
+++ src/test/java/com/learn/jpa/JpaApplicationTests.java
@@ -1,13 +1,21 @@
 package com.learn.jpa;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.*;
+import org.springframework.boot.test.context.*;
 
 @SpringBootTest
 class JpaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    // Testcontainers at development time - Refer to the following article for more details
+    // https://spring.io/blog/2023/06/23/improved-testcontainers-support-in-spring-boot-3-1
+
+    public static void main(String[] args) {
+        SpringApplication.from(JpaApplication::main).with(MyContainersConfiguration.class).run(args);
+    }
 
 }
